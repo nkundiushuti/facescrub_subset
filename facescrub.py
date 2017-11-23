87ea19d56cba373c526ddc087f5f9a81b3cae132
Marius Miron <miron.marius@gmail.com>
2017-11-23 14:53:45 +0100

    update

--- facescrub.py
+++ facescrub.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 
 #download Facescrub cropped from the official Megaface page: http://megaface.cs.washington.edu/participate/challenge.html and unzip in into path_to_facescrub
 
-maxPeople=50 #take a subset of the people with the most photos
+maxPeople=20 #take a subset of the people with the most photos
 path_to_facescrub = '/Volumes/HDD 1/Downloads/facescrub_aligned/'
 
 people = sorted([name for name in os.listdir(path_to_facescrub) if os.path.isdir(os.path.join(path_to_facescrub, name))])
@@ -25,8 +25,8 @@ countall = np.sum(fileCounter[selected])
 images=np.zeros((countall,3,32,32),dtype=np.int8)
 labels=np.zeros(countall,dtype=np.int8)
 k = 0
+i = 0
 for actid,act in enumerate(people):
-    i = 0
     if actid in selected:
         for img in glob.glob1(os.path.join(path_to_facescrub,act),"*.png"):
             rawimg = imread(os.path.join(path_to_facescrub,act,img),mode='RGB')
@@ -37,10 +37,15 @@ for actid,act in enumerate(people):
 
 images_train, images_test, labels_train, labels_test = train_test_split(images, labels, test_size=0.1, random_state=42)
 
+train={}
+train['features'] = images_train
+train['labels'] = labels_train
+test={}
+test['features'] = images_test
+test['labels'] = labels_test
+
 with open(os.path.join(path_to_facescrub,'facescrub_train_'+str(maxPeople)+'.pkl'), 'wb') as output:
-    pickle.dump(images_train, output, pickle.HIGHEST_PROTOCOL)
-    pickle.dump(labels_train, output, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(train, output, pickle.HIGHEST_PROTOCOL)
 
 with open(os.path.join(path_to_facescrub,'facescrub_test_'+str(maxPeople)+'.pkl'), 'wb') as output:
-    pickle.dump(images_test, output, pickle.HIGHEST_PROTOCOL)
-    pickle.dump(labels_test, output, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(test, output, pickle.HIGHEST_PROTOCOL)
